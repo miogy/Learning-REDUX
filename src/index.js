529704cd48d6ba3efd92d5424ef21cdad08450e7
miogy <jjangrl87@gmail.com>
2022-12-07 23:42:03 +0900

    ADD : todo-redux

--- src/index.js
+++ src/index.js
@@ -1,18 +1,36 @@
+import { createStore } from "redux";
+
 const form = document.querySelector("form");
 const input = document.querySelector("input");
 const ul = document.querySelector("ul");
 
-const createToDo = (toDo) => {
-  const li = document.createElement("li");
-  li.innerText = toDo;
-  ul.appendChild(li);
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
+
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return [];
+    case DELETE_TODO:
+      return [];
+    default:
+      return state;
+  }
 };
+const store = createStore(reducer);
+
+// const createToDo = (toDo) => {
+//   const li = document.createElement("li");
+//   li.innerText = toDo;
+//   ul.appendChild(li);
+// };
 
 const addBtn = (e) => {
   e.preventDefault();
   const toDo = input.value;
   input.value = "";
-  createToDo(toDo);
+  // createToDo(toDo);
+  store.dispatch({ type: ADD_TODO, text: toDo });
 };
 
 form.addEventListener("submit", addBtn);
